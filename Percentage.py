3e4a1f23e3a6971a3546133255088be7814d6354
SomeRandomPerson4112 <ayaangadani@gmail.com>
2024-11-09 19:03:02 +0500

    main.1234567

---
+++ Percentage.py
@@ -0,0 +1,9 @@
+print("enter marks obtained: ")
+math=int(input("math: "))
+science=int(input("science: "))
+english=int(input("english: "))
+urdu=int(input("urdu: "))
+sum=math + science + english + urdu
+print("the sum of the marks is: ",sum)
+percentage=(sum/400)*100
+print("percentage is: ",percentage)
